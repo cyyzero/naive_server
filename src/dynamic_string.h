1329982a02f2eeed461b6dc85649cbba53cab284
cyyzero <cyyzero16@gmail.com>
2020-11-19 01:23:00 +0800

    Revert "update http and libevent  base"
    This reverts commit bbc05bea302ea869abae13d163e6db01a0e1bc17.

--- src/dynamic_string.h
+++
@@ -1,8 +0,0 @@
-#ifndef DYNAMIC_STRING_H
-#define DYNAMIC_STRING_H
-
-#include "sds.h"
-
-typedef sds dynamic_string;
-
-#endif // DYNAMIC_STRING_H
--- src/server.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-
-struct options;
-extern void server_init(struct options* options);
-extern void server_run();
-extern void server_destroy();
-
-#endif // SERVER_H
